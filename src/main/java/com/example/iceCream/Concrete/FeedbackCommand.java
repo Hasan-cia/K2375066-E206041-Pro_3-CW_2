b5701de8853ade5ff6770e5fede8d724568159e9
Hasan-cia <ciahasan98@gmail.com>
2024-01-26 12:13:25 +0530

    Fixed the erro in flavour decor and feedback

--- src/main/java/com/example/iceCream/Concrete/FeedbackCommand.java
+++ src/main/java/com/example/iceCream/Concrete/FeedbackCommand.java
@@ -3,8 +3,16 @@ package com.example.iceCream.Concrete;
 import com.example.iceCream.Interfaces.Command;
 
 public class FeedbackCommand implements Command {
+    private DataStore dataStore;
+    private Feedback feedback;
+
+    public FeedbackCommand(Feedback feedback, DataStore dataStore)
+    {
+        this.feedback = feedback; this.dataStore = dataStore;
+    }
     @Override
     public void execute() {
-
+        System.out.println("Creating FeedBack");
+        this.dataStore.getFeedbacks().put(feedback.getId(),feedback);
     }
 }
